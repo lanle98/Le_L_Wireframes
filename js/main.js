4e7c194032faa6beef1f4d35373ee49b8e1efa2c
Darren Le <darrenle98@gmail.com>
2019-01-20 02:17:37 -0500

    added folders,  linked css and js files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// creat jar, name jar, place string in jar
+var firstName = "Lan";
+
+//alert is method (built in function in JS)
+// alert();
+
+//modern JS
+// alert(`Hello $(firstName)`)
+
+console.log("javascript linked up");
